778e77d55d82ba9c79c187e592cac9c088e1503d
NoamRI <noam.ri02gmail.com>
2017-11-27 11:39:46 +0200

    menu, main

--- Menu.cpp
+++ Menu.cpp
@@ -11,14 +11,14 @@ int Menu::choosePlayer() {
     int chose = 1; // defult chose.
     bool validInput = false;
 
-    cout << "Welcome to REVERSI!" << endl << endl<< "Please select the player you want to play with:" << endl
+    cout << endl << "Welcome to REVERSI!" << endl << endl<< "Please select the player you want to play with:" << endl
          << "AI opponent: 1" << endl << "Console opponent: 2" << endl;
 
     while(!validInput) {
         cin >> chose;
         if ((chose == ai) || (chose == console)) {
             validInput = true;
-            cout << "Here we go!!!!" << endl << endl;
+            cout << endl << "Here we go!!!!" << endl << endl;
         } else {
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
